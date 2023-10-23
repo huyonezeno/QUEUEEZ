2ca7fbd138778815f27488d6d278ea6a04cf7c1a
huyonezeno <90764815+huyonezeno@users.noreply.github.com>
2023-10-23 22:48:55 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -5,31 +5,38 @@ using namespace std;
 void print(queue<int>q) {
 	
 }
-int main() {
+int main() 
+{
 	queue<int>q;
 	int n;
 	scanf("%d",&n);
-	while (n--) {
-		int x, y;
-		scanf("%d",&x);
-		if (x == 1) {
-			scanf("%d",&y);
-			q.push(y);
+  while (n--)
+	{
+	int x, y;
+	scanf("%d",&x);
+	if (x == 1)
+	{
+		scanf("%d",&y);
+		q.push(y);
+        }
+	else if (x == 2)
+	{
+		if (!q.empty()) 
+		{
+		q.pop();
 		}
-		else if (x == 2) {
-			if (!q.empty()) {
-				q.pop();
-			}
+	}
+	else
+	{
+		if (!q.empty())
+		{
+		int z = q.front();
+		printf("%d\n",z);
 		}
-		else {
-			if (!q.empty()) {
-				int z = q.front();
-				printf("%d\n",z);
-			}
-			else {
-				
-					printf("Empty!\n");
-			}
+		else 
+		{		
+		printf("Empty!\n");
 		}
+	}
 	}
 }
