0b76968d1644c734d2de7568069b488c48b25c9f
huyonezeno <90764815+huyonezeno@users.noreply.github.com>
2023-10-23 22:36:46 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   int a[n+2];
+   for(int i=1;i<=n;i++){
+    cin>>a[i];
+   }
+   sort(a+1,a+n+1);
+   for(int i=1;i<=n;i++){
+        int l=i,
+        int r=i-1;
+        long long C=
+   }
+}
